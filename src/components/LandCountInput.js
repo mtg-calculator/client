d9e64ca2942ba261a07bbea10effbdf88a527b34
emily-yang <cigoua@gmail.com>
2019-09-22 18:37:48 -0700

    add land count input component

--- src/components/LandCountInput.js
+++ src/components/LandCountInput.js
@@ -1,14 +1,50 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ColorButton from './ColorButton';
+import InputNumber from 'rc-input-number';
+import 'rc-input-number/assets/index.css';
+import '../styles/LandCountInput.scss';
+
 
 export default class LandCountInput extends Component {
   static propTypes = {
     prop: PropTypes
   }
 
+  constructor() {
+    super();
+
+    this.state = {
+      count: 1,
+      turn: 1
+    }
+  }
+
+  onCountChange = count => {
+    this.setState({ count });
+  }
+
+  onTurnChange = turn => {
+    this.setState({ turn });
+  }
+
   render() {
     return (
-      <div>
+      <div className="land-count-input">
+        <InputNumber className="input-number count-needed-input"
+          min={1}
+          max={6}
+          value={this.state.count}
+          onChange={this.onCountChange}
+        />
+        <ColorButton color={this.props.color} />
+        <p>needed by turn </p>
+        <InputNumber className="input-number turn-needed-input"
+          min={1}
+          max={8}
+          value={this.state.turn}
+          onChange={this.onTurnChange}
+        />
 
       </div>
     )
