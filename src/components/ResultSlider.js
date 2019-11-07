dc5a4dd5fee1112893fea8cafb39a21041d264ab
emily-yang <cigoua@gmail.com>
2019-11-07 14:27:18 -0800

    display tapped and untapped values upon  slider input

--- src/components/ResultSlider.js
+++ src/components/ResultSlider.js
@@ -1,34 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Slider } from 'material-ui-slider';
 import '../styles/ResultSlider.scss';
 import { COLOR_CODES } from '../colors.js';
 
-// import DeckSizeSelector from './DeckSizeSelector';
-// import ColorsSelector from './ColorsSelector';
-// import LandInputsField from './LandInputsField';
-// import 'rc-input-number/assets/index.css';
-// import '../styles/InputForm.scss';
-// import { checkForm, formatSubmission } from '../utils/formUtils';
-
 const ResultSlider = ({data, color}) => {
   console.log("Data: ", {data, color})
-  const [value, setValue] = useState(0);
+  const [tapped, setTapped] = useState(0);
 
-  // const [deckSize, setDeckSize] = useState(null);
-  // const [colors, setColors] = useState({});
-
-  // useEffect(() => {
-  //   setErrors(checkForm({ deckSize, colors }));
-  // }, [deckSize, colors]);
 const handleChange = value => {
-  setValue(value);
+  setTapped(value);
+}
+
+const getUntappedValue = numTapped => {
+  if (numTapped < 0) return 'error';
+  return data[numTapped] || getUntappedValue(numTapped - 1);
 }
 
 
   return (
     <div className="result-slider">
-      <p>Value: {value}</p>
+      <p>Tapped: {tapped}</p>
+      <p>Untapped: {getUntappedValue(tapped)}</p>
       <Slider
        defaultValue={0}
        color={COLOR_CODES[color]}
