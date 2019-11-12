c604b1ac5047f1835d8dea9a8b9d2f177aec55e2
emily-yang <cigoua@gmail.com>
2019-11-12 13:31:50 -0800

    switch to material-ui library for sliders

--- src/components/ResultSlider.js
+++ src/components/ResultSlider.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
-import { Slider } from 'material-ui-slider';
+// import { Slider } from 'material-ui-slider';
 import ColorDot from './ColorDot';
+import Slider from '@material-ui/core/Slider';
 import '../styles/ResultSlider.scss';
+// import { makeStyles } from '@material-ui/core/styles;'
 import { COLOR_CODES } from '../colors.js';
 
 const ResultSlider = ({ data, color }) => {
   console.log('Data: ', { data, color });
   const [tapped, setTapped] = useState(0);
+  const marks = data.reduce((accum, curr, i) => {
+    if (curr !== null) accum.push({ value: i, label: i });
+    return accum;
+  }, []);
+
+  console.log('marks: ', marks);
 
   const handleChange = value => {
     setTapped(value);
@@ -28,12 +36,19 @@ const ResultSlider = ({ data, color }) => {
         shape="untapped"
         count={getUntappedValue(tapped)}
       />
-      <Slider
+      {/* <Slider
         defaultValue={0}
         color={COLOR_CODES[color]}
         min={0}
         max={data.length - 1}
         onChange={handleChange}
+      /> */}
+      <Slider
+        defaultValue={0}
+        marks={marks}
+        valueLabelDisplay="on"
+        max={marks[marks.length - 1].value}
+        step={null}
       />
       <p>Tapped: {tapped}</p>
       <ColorDot color={color} shape="tapped" count={tapped} />
