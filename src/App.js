86b56da28528403046d146de544e7d208d7c773f
emily-yang <cigoua@gmail.com>
2019-12-23 16:12:57 -0800

    add framing for app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { useState } from 'react';
+import { Paper } from '@material-ui/core';
 import './App.css';
 import Header from './components/Header';
 import InputForm from './components/InputForm';
 import ResultForm from './components/ResultForm';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  app: {
+    paddingTop: theme.spacing(5),
+    paddingBottom: theme.spacing(5),
+    textAlign: "center",
+    flexGrow: 1,
+  },
+  paper: {
+    height: "100%",
+  }
+}))
 
 function App() {
   const [results, setResults] = useState(null);
@@ -14,11 +28,15 @@ function App() {
     setResults(null);
   };
 
+  const classes = useStyles();
+
   return (
-    <div className="App">
+    <div className={classes.app}>
+      <Paper className={classes.paper} elevation={10} square="true">
       <Header />
       {!results && <InputForm onSubmit={handleInputSubmit} />}
       {results && <ResultForm results={results} onReset={handleResultReset} />}
+      </Paper>
     </div>
   );
 }
